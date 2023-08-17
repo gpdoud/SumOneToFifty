293b9f60ef472e0d7227d840fa90b1d475e6d207
Greg Doud <gpdoud@gmail.com>
2023-08-17 16:03:10 -0400

    Add project files.

---
+++ SumOneToFifty/Program.cs
@@ -0,0 +1,15 @@
+﻿var sum = 0;
+
+for (int idx = 1; idx <= 50; idx++) {
+
+    if(idx % 5 == 1 && idx % 7 == 1) {
+        continue;
+    }
+    sum = sum + idx;
+
+    //if (idx % 5 == 0 || idx % 7 == 0) {
+    //    sum = sum + idx;
+    //}
+}
+
+Console.WriteLine($"Sum of numbers 1 to 50 evenly divisible by 5 or 7 is {sum}");
